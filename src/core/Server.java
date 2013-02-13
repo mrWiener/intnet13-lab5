1a8b28fc33f7bb0f7419194fbe6dca8c4ffcb8a8
Lucas Wiener <lucas.wiener@trustly.com>
2013-02-13 16:06:26 +0100

    Server now working with SSL

--- src/core/Server.java
+++ src/core/Server.java
@@ -4,33 +4,26 @@ import javax.net.ssl.*;
 import java.io.*;
 
 public class Server {
-	public static void main(String[] args) {
-		new Certificate("/Users/Lucas/.keystore");
-		
-		SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-		
-		System.out.println("Stšder:");
-		for (int i = 0; i < ssf.getSupportedCipherSuites().length; i++)
-			System.out.println(ssf.getSupportedCipherSuites()[i]);
-		
-		SSLServerSocket ss = null;
+	public static void main(String[] args) throws IOException {
+		System.setProperty("javax.net.ssl.keyStore", System.getProperty("user.home") + File.separator + ".keystore");
+		System.setProperty("javax.net.ssl.keyStorePassword", "rootroot");
+		SSLServerSocketFactory factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+		SSLServerSocket ss = (SSLServerSocket) factory.createServerSocket(8888); // Create the ServerSocket
+
 		try {
-			ss = (SSLServerSocket) ssf.createServerSocket(8888);
-			String[] cipher = { "SSL_DH_anon_WITH_RC4_128_MD5" };
-			ss.setEnabledCipherSuites(cipher);
-			
 			System.out.println("Vald:");
 			for (int i = 0; i < ss.getEnabledCipherSuites().length; i++)
 				System.out.println(ss.getEnabledCipherSuites()[i]);
-			
+
 			SSLSocket s = (SSLSocket) ss.accept();
-			BufferedReader infil = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+			System.out.println("client connected!");
 			
-			String rad = null;
-			while ((rad = infil.readLine()) != null)
-				System.out.println(rad);
-			
-			infil.close();
+			BufferedWriter outfile =new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+			outfile.write("Hello World");
+			outfile.flush();
+			outfile.close();
+			s.close();
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
